a95f4b158539f88e3ca5e52484792e5c0ef8754c
letuu <albertvt@ya.ru>
2022-12-20 09:06:18 +0300

    Initial commit

---
+++ src/main/java/pro/sky/algorithmsintegerlist/AlgorithmsIntegerlistApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithmsintegerlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsIntegerlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsIntegerlistApplication.class, args);
+    }
+
+}
